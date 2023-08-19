40b22c1ef5e1b15ffe51295226b51976e98651fe
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-08-20 00:05:53 +0300

    update config

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,9 +1,19 @@
 local highlights = require("nord.plugins.bufferline").akinsho()
 
 require("bufferline").setup({
-    options = {
-        separator_style = "thin",
-    },
-    highlights = highlights,
+	options = {
+		separator_style = "slant",
+		indicator = {
+			icon = ' ',
+			style = 'icon',
+		},
+		modified_icon = '●',
+		buffer_close_icon = '󰅙',
+		close_icon = '',
+		left_trunc_marker = '',
+		right_trunc_marker = '',
+		diagnostics = "nvim_lsp",
+	},
+	highlights = highlights,
 })
 
